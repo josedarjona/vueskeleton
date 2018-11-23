dcb38a39a8951dee216cfcabe28b0e0d183e466e
jose arjona <josedarjona@gmail.com>
2018-11-23 00:59:30 -0500

    listens to backend

--- posts/client/src/App.vue
+++ posts/client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- dont need this logo -->
+    <!-- <img src="./assets/logo.png"> -->
     <router-view/>
   </div>
 </template>
--- posts/client/src/router/index.js
+++ posts/client/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import Hello from '@/components/Hello'
+import Posts from '@/components/Posts'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Hello',
+    //   component: Hello
+    // },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/posts',
+      name: 'Posts',
+      component: Posts
     }
   ]
 })
